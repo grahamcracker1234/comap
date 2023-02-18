1903b672311e4556fba250c8b64d8524cc9852e9
Graham Preston <g.b.preston@proton.me>
2023-02-18 13:17:35 -0500

    Updated main

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ import numpy as np
 from collections import Counter
 import string
 
-def total_char_freq(df, display=False):
-    chars: list[str] = [''] * (len(df.index) * 5)
+def index_of_char(char: str) -> int:
+    if char.isupper():
+        return ord(char) - ord("A")
+    return ord(char) - ord("a")
 
-    i = 0
+def total_char_freq(df, display=False):
+    freq = np.array([0] * 26)
+
     for word in df.word:
         for char in word:
-            chars[i] = char
-            i += 1
-
-    freq = Counter(chars)
+            freq[index_of_char(char)] += 1
 
     if display:
-        freq_df = pd.DataFrame(freq.values(), columns=["count"], index=freq.keys())
+        freq_df = pd.DataFrame(freq, index=list(string.ascii_lowercase), columns=["count"])
         freq_df.sort_index(ascending=True, inplace=True)
         freq_df.plot.bar()
-        plt.show()
     
     return freq
     
@@ -27,20 +27,24 @@ def positional_char_freq(df, display=False):
     freq = np.array([[0] * 5] * 26)
     for word in df.word:
         for j, char in enumerate(word):
-            freq[ord(char) - ord("a")][j] += 1
+            freq[index_of_char(char)][j] += 1
             
     freq_df = pd.DataFrame(freq, index=list(string.ascii_lowercase), columns=["1st", "2nd", "3rd", "4th", "5th"])
     
     if display:
         freq_df.plot.bar()
-        plt.show()
         
     return freq_df
     
 if __name__ == "__main__":
     df = pd.read_csv("./data.csv", parse_dates=["date"])
-    freq = total_char_freq(df, display=False)
+    freq = total_char_freq(df, display=True)
     print(freq)
     freq = positional_char_freq(df, display=True)
     print(freq)
+    print(len(df.word))
+    print([l < 5 for word in df.word if len(set(list(word))) < 5])
+    print(sum(l < 5 for l in [len(set(list(word))) for word in df.word]))
+    # plt.show()
+    
     
